c7cf327ddd73305b6261561d2030447eb29209f1
gonzalezben81 <gonzalezben393@gmail.com>
2025-11-08 06:22:33 +0000

    Updated

--- variables.tf
+++ variables.tf
@@ -1,6 +1,34 @@
-variable "do_token" {}
-variable "plex_ssh_key_name" {}
-variable "droplet_name" {}
-variable "droplet_region" {}
-variable "droplet_size" {}
-variable "droplet_image" {}
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "plex_ssh_key_name" {
+  description = "Name of the SSH key registered in DigitalOcean"
+  type        = string
+}
+
+variable "droplet_name" {
+  description = "Name of the Plex droplet"
+  type        = string
+  default     = "plex-media-server"
+}
+
+variable "droplet_region" {
+  description = "DigitalOcean region for deployment"
+  type        = string
+  default     = "nyc3"
+}
+
+variable "droplet_size" {
+  description = "Droplet size slug (e.g., s-1vcpu-1gb)"
+  type        = string
+  default     = "s-1vcpu-1gb"
+}
+
+variable "droplet_image" {
+  description = "Droplet image slug (e.g., ubuntu-22-04-x64)"
+  type        = string
+  default     = "ubuntu-22-04-x64"
+}
